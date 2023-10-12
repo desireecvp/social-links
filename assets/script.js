5bb69d6bf2f1a3421db24176d049f8bb1422ce16
Desiree <desireecamillevieira@gmail.com>
2023-10-12 17:30:12 +0200

    profile pictures uptaded

--- assets/script.js
+++ assets/script.js
@@ -9,13 +9,10 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute("alt", "foto light mode")
+    img.setAttribute("alt", "Foto Desiree sorrindo")
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute(
-      "alt",
-      "Foto de fundo preto e metade de um corpo de escutura branca."
-    )
+    img.setAttribute("src", "./assets/Avatar.png")
+    img.setAttribute("alt", "Foto Desiree sorrindo de oculos escuro.")
   }
 }
